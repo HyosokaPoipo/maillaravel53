e4195d90f17c8559e70ad9e3aee1a85b5184a4a2
Maryadi <maryadi@astrnt.co>
2016-09-03 17:16:19 +0700

    Finishing part II

--- app/Http/Controllers/emailcontroller.php
+++ app/Http/Controllers/emailcontroller.php
@@ -14,12 +14,12 @@ class emailcontroller extends Controller
         Log::info("Request cycle without Queues started");
 
         Mail::send('emailcontent', ['data'=>'data'], function ($message) {
-
+            $message->subject("Default Email");
             $message->from('adiatstei07@gmail.com', 'Hisoka');
-
             $message->to('hyosoka187@gmail.com');
+            $message->cc('maryadi@astrnt.co');
+            $message->attachData('This is my pdf Data...','hisoka.pdf');
             log::info("End of mail processing...");
-
         });
 
 
